05f0b7e8b86e9065ba951b0f57e3870ea2b4d1e4
Elena Mylläri <elena.myllari@metropolia.fi>
2020-02-21 16:26:26 +0200

    database

--- src/server/dbconfig.js
+++ src/server/dbconfig.js
@@ -1,9 +1,14 @@
 module.exports = {
 
     database:{
-        host: "localhost",
-        user: "test",
-        password: "test",
-        database: "painikepeli"
+        //cleardb-corrugated-36622
+        host: "eu-cdbr-west-02.cleardb.net",
+       // host: "localhost",
+        user: "bbf7a8c692fa1b",
+        password: "4e82feaa",
+        database: "heroku_dfdf1635233775b"
+        //user: "test",
+        //password: "test",
+        //database: "painikepeli"
     }
 };
