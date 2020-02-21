5a945feba30a82a9edebd94ac0663a0570da98e0
Elena Mylläri <elena.myllari@metropolia.fi>
2020-02-21 13:03:57 +0200

    port

--- src/server/server.js
+++ src/server/server.js
@@ -26,6 +26,7 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = process.env.PORT || 3000;
     //var port = server.address().port;
+    //
     /* eslint-disable no-console */
     console.log("Example app listening at http://%s:%s", host, port)
     /* eslint-enable no-console */
