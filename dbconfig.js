7f47ed0a0dbf045551297a3cce63d88af1d1538b
Elena Mylläri <elena.myllari@metropolia.fi>
2020-02-24 08:42:36 +0200

    cleaning and commenting the code

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-        host: "eu-cdbr-west-02.cleardb.net",
-        user: "bbf7a8c692fa1b",
-        password: "4e82feaa",
-        database: "heroku_dfdf1635233775b"
+        host: "",
+        user: "",
+        password: "",
+        database: ""
 };
